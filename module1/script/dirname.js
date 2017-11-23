07b7b6e2d8356592fce884ffcafda92a78fdcf58
monpeco <monpeco@gmail.com>
2017-11-23 01:53:47 +0000

    Add Node.js globals examples

---
+++ module1/script/dirname.js
@@ -0,0 +1,11 @@
+console.log("__dirname: \t" + __dirname);
+console.log("__filename: \t" + __filename);
+console.log("process.cwd(): \t" + process.cwd());
+
+/*
+Output:
+
+__dirname:      /home/ubuntu/workspace/intro-node/module1/script
+__filename:     /home/ubuntu/workspace/intro-node/module1/script/dirname.js
+process.cwd():  /home/ubuntu/workspace/intro-node
+*/
---
+++ module1/script/eval_env.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env
+
+echo "console.log(process.pid)"
+node -e "console.log(process.pid);"
+
+echo "console.log(process.env)"
+node -e "console.log(process.env);"
+
