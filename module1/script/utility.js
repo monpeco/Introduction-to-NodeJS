36cdfccfe16e71a10b8955dc685416a24e192f6d
monpeco <monpeco@gmail.com>
2017-11-24 12:22:40 +0000

    Remove console.log

--- module1/script/utility.js
+++ module1/script/utility.js
@@ -2,13 +2,9 @@ module.exports = function(numberToSum){
     var sum = 0,
         i = 0,
         lenght = numberToSum.length;
-        
-        //console.log(numberToSum.l);
-
+    
     while (i < lenght){
         sum += numberToSum[i];
-        //console.log(sum);
-
         i++;
     }
         
