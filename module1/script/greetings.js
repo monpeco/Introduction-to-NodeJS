15643b81a0e5b6313ef1c27e8dc3ce231808e559
monpeco <monpeco@gmail.com>
2017-11-24 19:08:39 +0000

    Add NodePatterns for module.exports example

---
+++ module1/script/greetings.js
@@ -0,0 +1,11 @@
+exports.sayHelloEnglish = function(){
+	return 'Hello';
+}
+
+exports.sayHelloSwedish = function(){
+	return 'Hej';
+}
+
+exports.sayHelloTartar = function(){
+	return 'Isänme';
+}
---
+++ module1/script/printGreetings.js
@@ -0,0 +1,5 @@
+var greetings = require('./greetings');
+
+console.log("English: " + greetings.sayHelloEnglish() +
+            ", Swedish: " + greetings.sayHelloSwedish() + 
+            ", Tartar: " + greetings.sayHelloTartar());
