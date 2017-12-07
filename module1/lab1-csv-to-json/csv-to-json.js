f03dd178faf8218c0238a14d35ecd0e5bddb6845
monpeco <monpeco@gmail.com>
2017-12-07 17:22:07 +0000

    Fix writeFileSync and change const csv to csvtojson

--- module1/lab1-csv-to-json/csv-to-json.js
+++ module1/lab1-csv-to-json/csv-to-json.js
@@ -1,17 +1,15 @@
 const fs = require('fs');
 const path = require('path');
-const csvFilePath = path.join(__dirname,'customer-data.csv');
-const csv = require('csvtojson');
+const csvtojson = require('csvtojson');
+const csvFile = path.join(__dirname,'customer-data.csv');
 
-csv()
-  .fromFile(csvFilePath)
+
+csvtojson()
+  .fromFile(csvFile)
   .on('end_parsed', (jsonObj) => {    
-    fs.writeFileSync(path.join(__dirname,'test.txt'), JSON.stringify(jsonObj, null, 2), (error) => {
-      if(error){
-        return console.log(err.message);
-      }
-    });
-  })
-  .on('end', (error) => {
+    fs.writeFileSync(path.join(__dirname, 'output.txt'), JSON.stringify(jsonObj, null, 2));
+  }).on('end', () => {
     console.log('End');
+  }).on('error', (error) => {
+    console.log(`Error: ${error}`);
   });
