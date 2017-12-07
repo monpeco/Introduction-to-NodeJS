c21dc8d0cd1755e0e5b7919c2bbd37dfbb33a002
monpeco <monpeco@gmail.com>
2017-12-06 23:23:59 -0300

    Add end_parsed and stringify

--- module1/lab1-csv-to-json/csv-to-json.js
+++ module1/lab1-csv-to-json/csv-to-json.js
@@ -1,9 +1,17 @@
 const fs = require('fs');
 const path = require('path');
+const csvFilePath = path.join(__dirname,'customer-data.csv');
+const csv = require('csvtojson');
 
-fs.writeFileSync(path.join(__dirname,'test.txt'), 'Hey!', (error) => {
-  if(error){
-    return console.log(err.message);
-  }
-  console.log("The file was saved!");
-});
+csv()
+  .fromFile(csvFilePath)
+  .on('end_parsed', (jsonObj) => {    
+    fs.writeFileSync(path.join(__dirname,'test.txt'), JSON.stringify(jsonObj, null, 2), (error) => {
+      if(error){
+        return console.log(err.message);
+      }
+    });
+  })
+  .on('end', (error) => {
+    console.log('End');
+  });
