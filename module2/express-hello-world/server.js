f59324290bee0529dc6d22b94fc4c852f9e0a1f1
monpeco <monpeco@gmail.com>
2017-12-11 00:44:02 -0300

    Add logging to root

--- module2/express-hello-world/server.js
+++ module2/express-hello-world/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello world from Express');
+  console.log('Root in Express!!');
+  res.send('Hello world from Express!!');
 })
 
 app.listen(3000);
