1ad18cb8c4fa6f4a10ec2cbd7bac4b46642d19df
monpeco <monpeco@gmail.com>
2017-12-11 17:12:59 +0000

    Add middleware example (chaining, passing data)

---
+++ module2/middleware/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express()
+
+const middleware = (request, response, next) => {
+  request.pm = 'This is a parameter passing';
+  console.log('Middleware!!');
+  next()
+}
+
+app.use(middleware);
+
+app.use((request, response, next)=>{
+  console.log("request.pm: " + request.pm);
+  console.log("Another Middleware!!");
+  next();
+});
+
+app.get('/', (req, res) => {
+  console.log('Root in Express!!');
+  console.log("(In the request handler) req.pm: " + req.pm);
+  res.send('Hello world from Express!! (with middleware)');
+})
+
+app.listen(process.env.PORT);
