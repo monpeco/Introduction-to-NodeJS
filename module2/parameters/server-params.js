48aa3945f8c1b6509463f265e2158ea667f169fb
monpeco <monpeco@gmail.com>
2017-12-14 12:43:38 +0000

    Add get method with multiple parameters in the URL

--- module2/parameters/server-params.js
+++ module2/parameters/server-params.js
@@ -8,6 +8,12 @@ app.get('/accounts/:id/', (req, res) => {
   res.send(`parameter send: ${accountId}\n`);
 });
 
+app.get('/accounts/:id/transactions/:transactionId',(req, res) => {
+  var accountId = req.params.id;
+  var transactionId = req.params.id;
+  console.log(`Parameters send: \nid:${accountId}\ntransactionId: ${transactionId}\n`);
+  res.send(`Parameters send: \nid: ${accountId}\ntransactionId: ${transactionId}\n`);
+});
 
 app.listen(myPort);
 
@@ -17,5 +23,12 @@ console.log(`running on port ${myPort}`);
 curl GET localhost:8080/accounts/12
 
 parameter send: 12
+
+--
+curl GET localhost:8080/accounts/14654/transactions/1244774
+
+Parameters send: 
+id: 14654
+transactionId: 14654
 
 */
