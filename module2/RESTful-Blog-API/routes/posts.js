0885da8b9704a8efb2bec90db142a47d13f2808c
monpeco <monpeco@gmail.com>
2017-12-24 01:44:16 +0000

    Add DELETE posts

--- module2/RESTful-Blog-API/routes/posts.js
+++ module2/RESTful-Blog-API/routes/posts.js
@@ -19,8 +19,8 @@ module.exports.myPut = function(req, res){
 }
 
 module.exports.myDelete = function(req, res){
-  res.status(200).send('resource: posts, method: DELETE\n');
+  index.store.accounts.splice(req.params.id,1);
   index.Counter.remove();
-
+  res.status(200).send(`posts: ${req.params.id} deleted\n`);
 }
 
