4f360f2783b24682d4fa43bba92b969beea21014
monpeco <monpeco@gmail.com>
2017-12-24 00:38:38 +0000

    Add id to POST and udpate on id in PUT

--- module2/RESTful-Blog-API/routes/posts.js
+++ module2/RESTful-Blog-API/routes/posts.js
@@ -8,13 +8,14 @@ module.exports.myGet = function(req, res){
 }
 
 module.exports.myPost = function(req, res){
-  res.status(200).send('resource: posts, method: POST\n');
+  res.status(200).send(`resource: posts, method: POST, id: ${index.Counter.count}\n`);
   index.Counter.add();
   index.store.accounts.push(req.body);
 }
 
 module.exports.myPut = function(req, res){
   res.status(200).send('resource: posts, method: PUT\n');
+  index.store.accounts[req.params.id] = req.body;
 }
 
 module.exports.myDelete = function(req, res){
