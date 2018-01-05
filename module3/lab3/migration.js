22ec209d5f1f53e49d0f426dcdf1f2a9cd435a43
monpeco <monpeco@gmail.com>
2018-01-05 19:42:35 +0000

    Add insert to collection customers

--- module3/lab3/migration.js
+++ module3/lab3/migration.js
@@ -4,12 +4,26 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017/edx-course-db'
 
 MongoClient.connect(url, (error, client) => {
-  console.log('Connecting ...')
   
-  if (error) return process.exit(1)
-  
-  console.log('Connection is okay')
+  console.log('Connecting ...');
+  if (error) return process.exit(1);
 
+  console.log('Connection is okay')
+  var db = client.db('customerDB'); 
+  const collection = db.collection('customers');
   
-})
+  collection.insert([
+    {jam : 'this is my jam'}
+  ], (error, result) => {
+    if (error) { 
+      console.log('error in insert');
+      return process.exit(1);
+    }
+    console.log('result.result.n :' + result.result.n);
+    console.log('result.ops.length: ' + result.ops.length);
+  });
+  
+  client.close();
+  
+});
 
