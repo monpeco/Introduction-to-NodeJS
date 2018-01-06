ff6c6da9b9bd5dfbc35ce4b7da4dba4ffd38c843
monpeco <monpeco@gmail.com>
2018-01-06 22:23:34 +0000

    Add asyncParallel() to run parallel tasks

--- module3/lab3/migration.js
+++ module3/lab3/migration.js
@@ -15,6 +15,28 @@ console.log(`taskCount: ${taskCount}`);
 
 const url = 'mongodb://localhost:27017/edx-course-db'
 
+
+
+var asyncParallel = function(tasks, callback) {
+  var results = [];
+  var count = tasks.length;
+  tasks.forEach(function(task, index) {
+    task(function(err, data) {
+      results[index] = data;
+      if (err) {
+        callback && callback(err);
+        callback = null;
+      }
+      if (--count === 0 && callback) {
+        callback(null, results);
+      }
+    });
+  });
+};
+
+
+
+
 MongoClient.connect(url, (error, client) => {
   
   console.log('Connecting ...');
@@ -34,6 +56,19 @@ MongoClient.connect(url, (error, client) => {
     console.log('result.result.n :' + result.result.n);
     console.log('result.ops.length: ' + result.ops.length);
   });
+  
+  
+  asyncParallel([
+    function() {
+      console.log('one');
+    },
+    function(cb) {
+      console.log('two');
+    }
+  ], function(err, results) {
+    if (err) return console.log(err);
+    console.log(results);
+  });  
   
   client.close();
   
