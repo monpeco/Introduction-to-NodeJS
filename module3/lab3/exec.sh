eff90aa80ee793cd6f28950be3b863d1fc9a5c89
monpeco <monpeco@gmail.com>
2018-01-07 01:16:21 +0000

    Add batch parameter and comments

--- module3/lab3/exec.sh
+++ module3/lab3/exec.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+# First delete.js is executed to delete all documents from db
 node delete.js
 
-node migration.js 100
+# Then, migration is executed to insert in db
+# batch is the size of the batch, that determinates how many documents are inserted in each task
+batch=100
+
+node migration.js $((batch))
 
