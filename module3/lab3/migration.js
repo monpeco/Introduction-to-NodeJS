44f42435cafd08c0473ac1776f34b95665d96470
monpeco <monpeco@gmail.com>
2018-01-07 01:17:08 +0000

    Move newTask and pass collection parameter

--- module3/lab3/migration.js
+++ module3/lab3/migration.js
@@ -9,6 +9,24 @@ const taskCount = customerData.length / batch;
 console.log(`batch: ${batch}, customerData.length: ${customerData.length}, taskCount: ${taskCount}`);
 
 
+function newTask(collection, start, end){
+  console.log(`loding data from : {start: ${start}, end: ${end} }`);
+  
+  let customerResults = [];   // Array that contains customers data to be inserted
+
+  for (let i=start; i<=end; i++){
+    let customerResult = Object.assign({},customerData[i], customerAddresses[i]);  // Join both json
+    customerResults.push(customerResult);
+  }
+
+  collection.insertMany( customerResults, (error, result) => {
+    if (error) return process.exit(1);
+    
+    console.log('result.ops.length: ' + result.ops.length);
+  });
+}
+  
+
 MongoClient.connect(url, (error, client) => {
   if (error) return process.exit(1);
 
@@ -18,26 +36,8 @@ MongoClient.connect(url, (error, client) => {
   
   let tasks = [];   // Array that contains the tasks to be executed (newTask's)
   
-  function newTask(start, end){
-    console.log(`loding data from : {start: ${start}, end: ${end} }`);
-    
-    let customerResults = [];   // Array that contains customers data to be inserted
-
-    for (let i=start; i<=end; i++){
-      let customerResult = Object.assign({},customerData[i], customerAddresses[i]);  // Join both json
-      customerResults.push(customerResult);
-    }
- 
-    collection.insertMany( customerResults, (error, result) => {
-      if (error) return process.exit(1);
-      
-      console.log('result.ops.length: ' + result.ops.length);
-    });
-  }
-
-  for (let i=0; i<taskCount; i++){
-    tasks.push(function(callback){ callback(null,newTask(i*batch, (i+1)*batch-1))});
-  }
+  for (let i=0; i<taskCount; i++)
+    tasks.push(function(callback){ callback(newTask(collection, i*batch, (i+1)*batch-1))});
 
   async.parallel(tasks, function(err, results) {
     if (err) return console.log(err);
