00db81bfd1de901019d06e284805b3e25669bd56
monpeco <monpeco@gmail.com>
2018-01-19 12:31:46 +0000

    Fix saving and add schemas

--- module4/tutorial/fetch-blog-posts.js
+++ module4/tutorial/fetch-blog-posts.js
@@ -2,47 +2,49 @@ let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/test01');
 
-const Post = mongoose.model('Post', {
+// Post Schema and Model
+const postSchema = mongoose.Schema({
   name: String,
   text: String,
-  url: String/*,
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]*/
+  url: String,
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
 });
+let Post = mongoose.model('Post', postSchema);
 
-const Comment = mongoose.model('Comment', {
+// Comment Schema and Model
+const commentSchema = mongoose.Schema({
   text: String
 });
+let Comment = mongoose.model('Comment', commentSchema);
 
+// Create an array of Coments
 let commentsArray = [
     {text: 'Cruel…..var { house, mouse} = No type optimization at all'},
     {text: 'I think you’re undervaluing the benefit of ‘let’ and ‘const’.'},
     {text: '(p1,p2)=>{ … } ,i understand this ,thank you !'}
-  ].map( (comment) => {
-    //console.log(comment);
-    const comm = new Comment(comment);
-    comm.save();
-    return comm._id;
-});
-//console.log(commentsArray);
+  ].map( (comment)=> {
+    const _comment = new Comment(comment);
+    _comment.save();
+    return _comment._id;
+  });
 
-let post = new Post({
+// Create a Post (with the comments array)
+let praticalPost = new Post({
   name: 'Top 10 ES6 Features every Web Developer must know',
   text: 'https://webapplog.com/es6',
   url: 'This essay will give you a quick introduction to ES6. If you don’t know what is ES6, it’s a new JavaScript implementation.',
   comments: commentsArray
 });
-//console.log(post);
+console.log(praticalPost);
 
-post.save(function(err){
-console.log('Script');
-
+praticalPost.save((err, result) => {
   if(err){
     console.log('error: ' );
+    process.exit(1);
   }else{
     console.log('Post saved: ' );
+    process.exit(0);
   }
 });
 
-console.log('Script end Ok');
-
-process.exit(0);
+console.log('End Script');
