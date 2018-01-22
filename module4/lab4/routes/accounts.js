1455ef76dfbb5f2324f23c873efcfff1877062e9
monpeco <monpeco@gmail.com>
2018-01-22 16:02:19 +0000

    Add account finded to get response

--- module4/lab4/routes/accounts.js
+++ module4/lab4/routes/accounts.js
@@ -1,9 +1,16 @@
 let mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/lab4');
 
+let Account = mongoose.model('Account', {name: String, balance: Number});
 
 module.exports.get = (req, res) => {
-  res.status(200).send('account.js - getAccount!!!\n');
+  
+  Account.find({},{_id:0},(err, _accounts)=>{
+    if(err) res.status(200).send('Internal error');
+    console.log(_accounts);
+    res.status(200).send(_accounts);
+  });
+
 }
 
 module.exports.post = function(req, res){
@@ -12,7 +19,7 @@ module.exports.post = function(req, res){
   let _balance = parseInt(req.body.balance);
   console.log(_name);
   console.log(_balance);
-  let Account = mongoose.model('Account', {name: String, balance: Number});
+  
   let practicalNodeAccount = new Account({ name: _name, balance: _balance });
   
   practicalNodeAccount.save((err,result) => {
